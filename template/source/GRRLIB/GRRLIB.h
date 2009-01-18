f5d25a55a5c0be9b9e02f54e3d34899fc7c6baee
Crayon2000 <crayon1@rocketmail.com>
2009-01-18 17:59:43 +0000

    [CHG] Code formatting and some doxygen comments

--- template/source/GRRLIB/GRRLIB.h
+++ template/source/GRRLIB/GRRLIB.h
@@ -16,15 +16,27 @@
 
 #include "../libpng/pngu/pngu.h"
 
+/**
+ * @struct GRRLIB_texImg
+ * @brief Structure to hold the texture informations.
+ * @param w width of the texture.
+ * @param h height of the texture.
+ * @param tilew widht of a tile.
+ * @param tileh height of a tile.
+ * @param nbtilew number of tiles for the x axis.
+ * @param nbtileh number of tiles for the y axis.
+ * @param tilestart
+ * @param data pointer to the texture data.
+ */
 typedef struct GRRLIB_texImg{
-	unsigned int w;
-	unsigned int h;
-	unsigned int tilew;
-	unsigned int tileh;
-	unsigned int nbtilew;
-	unsigned int nbtileh;
-	unsigned int tilestart;
-	void *data;
+    unsigned int w;
+    unsigned int h;
+    unsigned int tilew;
+    unsigned int tileh;
+    unsigned int nbtilew;
+    unsigned int nbtileh;
+    unsigned int tilestart;
+    void *data;
 } GRRLIB_texImg;
 
 extern Mtx GXmodelView2D;
@@ -50,7 +62,6 @@ inline void GRRLIB_DrawImg(f32 xpos, f32 ypos, GRRLIB_texImg tex, float degrees,
 inline void GRRLIB_DrawTile(f32 xpos, f32 ypos, GRRLIB_texImg tex, float degrees, float scaleX, f32 scaleY, u32 color, int frame);
 
 void GRRLIB_Printf(f32 xpos, f32 ypos, GRRLIB_texImg tex, u32 color, f32 zoom, char *text,...);
-
 
 
 void GRRLIB_GXEngine(Vector v[], u32 color, long count,u8 fmt);
--- template/source/main.c
+++ template/source/main.c
@@ -26,7 +26,7 @@
 Mtx GXmodelView2D;
 
 int main(){
-int rot=0;
+    int rot = 0;
 
     GRRLIB_InitVideo();
     GRRLIB_Start();
@@ -50,25 +50,24 @@ int rot=0;
 
 
     while(1){
-	WPAD_SetVRes(0, 640, 480);
+        WPAD_SetVRes(0, 640, 480);
         WPAD_ScanPads();
-        u32 wpaddown=WPAD_ButtonsDown(0);
-	u32 wpadheld=WPAD_ButtonsHeld(0);
+        u32 wpaddown = WPAD_ButtonsDown(0);
+        u32 wpadheld = WPAD_ButtonsHeld(0);
 
-
-	ir_t ir1; 
-	WPAD_IR(WPAD_CHAN_0, &ir1); 
+        ir_t ir1;
+        WPAD_IR(WPAD_CHAN_0, &ir1);
 
         GRRLIB_FillScreen(0x000000FF);
-	GRRLIB_Printf(rot, 200, tex_BMfont1, 0xFFFFFFFF, 1, "X VALUE:%d",(int)ir1.sx);
-	GRRLIB_Printf(rot, 250, tex_BMfont4, 0xFFFFFFFF, 1, "X VALUE:%d",(int)ir1.sx);
-	GRRLIB_Printf(rot, 300, tex_BMfont3, 0xFFFFFFFF, 1 , "X VALUE  : 1");
-	GRRLIB_Printf(rot, 350, tex_BMfont2, 0xFFFFFFFF, 1 , "X VALUE  : 1");
-	
+        GRRLIB_Printf(rot, 200, tex_BMfont1, 0xFFFFFFFF, 1, "X VALUE:%d",(int)ir1.sx);
+        GRRLIB_Printf(rot, 250, tex_BMfont4, 0xFFFFFFFF, 1, "X VALUE:%d",(int)ir1.sx);
+        GRRLIB_Printf(rot, 300, tex_BMfont3, 0xFFFFFFFF, 1 , "X VALUE  : 1");
+        GRRLIB_Printf(rot, 350, tex_BMfont2, 0xFFFFFFFF, 1 , "X VALUE  : 1");
+
         GRRLIB_Render();
 
         if (wpaddown & WPAD_BUTTON_A) exit(0);
-	if (wpadheld & WPAD_BUTTON_B) rot++;
+        if (wpadheld & WPAD_BUTTON_B) rot++;
     }
     return 0;
 }
