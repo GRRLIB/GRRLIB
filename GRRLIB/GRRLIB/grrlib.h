fff851b34bdda67dc2a70fc5a11260d7a6a4433f
Crayon2000 <crayon1@rocketmail.com>
2009-10-16 20:59:08 +0000

    [CHG] Deleted the Temporary fix (-DNOGUFIX)

--- GRRLIB/GRRLIB/grrlib.h
+++ GRRLIB/GRRLIB/grrlib.h
@@ -34,34 +34,7 @@ THE SOFTWARE.
 // Includes
 //==============================================================================
 #include <gccore.h>
-
 //==============================================================================
-/*
-*** Temporary fix ***
-After the release of libogc v1.7.1a, Vector was renamed to guVector
-"to avoid collisions" [svn 3650]
-...also Quaternion was renamed to guQuaternion - but GRRLIB does not use these!
-
-The main codebase of GRRLIB has been updated to reflect this change.
-But until the new libogc is officially released,
-if you are using a version of libogc later than v1.7.1a/svn3649,
-you will need to add:
-  -DNOGUFIX
-to the compiler flags in your makefile
-
-When the libogc changes are officially released,
-this *temporary fix* should be removed
-The requirement for -DNOGUFIX will be deprecated,
-but its lingering presence will not be a hinderance
-
-Thanks to Nicksasa for reporting this problem :)
-*/
-//==============================================================================
-#ifndef NOGUFIX
-// If you are getting a compile error here, read the above comment!
-typedef  Vector      guVector ;
-typedef  Quaternion  guQuaternion ;
-#endif
 
 //==============================================================================
 // C++ header
