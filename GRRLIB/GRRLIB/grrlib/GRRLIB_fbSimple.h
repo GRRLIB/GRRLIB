01adcdf9b9a91b4919a1e4570b5ed383a6d3eaf9
Crayon2000 <crayon1@rocketmail.com>
2009-10-20 22:21:01 +0000

    [CHG] math.h is not needed in GRRLIB_fbSimple.h

--- GRRLIB/GRRLIB/grrlib/GRRLIB_fbSimple.h
+++ GRRLIB/GRRLIB/grrlib/GRRLIB_fbSimple.h
@@ -25,8 +25,6 @@ THE SOFTWARE.
  * Inline functions for primitive point and line drawing.
  */
 
-#include <math.h>
-
 /**
  * Clear screen with a specific color.
  * @param color The color to use to fill the screen.
@@ -48,7 +46,7 @@ void  GRRLIB_FillScreen (const u32 color) {
 INLINE
 void  GRRLIB_Plot (const f32 x,  const f32 y,  const u32 color) {
     GX_Begin(GX_POINTS, GX_VTXFMT0, 1);
-        GX_Position3f32(x, y,  0);
+        GX_Position3f32(x, y, 0);
         GX_Color1u32(color);
     GX_End();
 }
@@ -66,7 +64,7 @@ INLINE
 void  GRRLIB_Line (const f32 x1, const f32 y1,
                    const f32 x2, const f32 y2, const u32 color) {
     GX_Begin(GX_LINES, GX_VTXFMT0, 2);
-        GX_Position3f32(x1, y1,  0);
+        GX_Position3f32(x1, y1, 0);
         GX_Color1u32(color);
         GX_Position3f32(x2, y2, 0);
         GX_Color1u32(color);
