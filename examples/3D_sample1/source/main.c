d40b41e2eaa037a9e50f05d5a29a2cb2d14e9528
N0NameN0 <alonetrio@yahoo.com>
2009-12-06 18:41:29 +0000

    [CHG] Remove old noname0X sample codes [ADD] Simple flat shaded cube sample code.

---
+++ examples/3D_sample1/source/main.c
@@ -0,0 +1,101 @@
+/*===========================================
+    NoNameNo Simple 3D cube Sample Code
+============================================*/
+#include <grrlib.h>
+
+#include <stdlib.h>
+#include <math.h>
+#include <malloc.h>
+#include <wiiuse/wpad.h>
+
+extern GXRModeObj *rmode;
+
+int main() {
+float a=0;
+u32 col[3];
+col[0]=0xFFFFFFFF;
+col[1]=0xAAAAAAFF;
+col[2]=0x666666FF;
+int cubeZ=0;
+
+    GRRLIB_Init();
+    WPAD_Init();
+
+    GRRLIB_Settings.antialias = true;
+
+    GRRLIB_setBackgroundColour(0x00, 0x00, 0x00, 0xFF);
+    GRRLIB_camera3dSettings(0.0f,0.0f,13.0f, 0,1,0, 0,0,0);
+
+    while(1) {
+	GRRLIB_2dMode();
+        WPAD_ScanPads();
+        if(WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) exit(0);
+        if(WPAD_ButtonsHeld(0) & WPAD_BUTTON_A) cubeZ++;
+        if(WPAD_ButtonsHeld(0) & WPAD_BUTTON_B) cubeZ--;
+
+	GRRLIB_3dMode(0.1,1000,45,0);
+	GRRLIB_objectView(0,0,cubeZ, a,a*2,a*3);
+	GX_Begin(GX_QUADS, GX_VTXFMT0, 24);
+	    GX_Position3f32(-1.0f,1.0f,-1.0f);
+	    GX_Color1u32(col[0]);
+	    GX_Position3f32(-1.0f,-1.0f,-1.0f);
+	    GX_Color1u32(col[0]);
+	    GX_Position3f32(1.0f,-1.0f,-1.0f);
+	    GX_Color1u32(col[0]);
+	    GX_Position3f32(1.0f,1.0f,-1.0f);
+	    GX_Color1u32(col[0]);
+
+	    GX_Position3f32(-1.0f,1.0f,1.0f);
+	    GX_Color1u32(col[0]);
+	    GX_Position3f32(-1.0f,-1.0f,1.0f);
+	    GX_Color1u32(col[0]);
+	    GX_Position3f32(1.0f,-1.0f,1.0f);
+	    GX_Color1u32(col[0]);
+	    GX_Position3f32(1.0f,1.0f,1.0f);
+	    GX_Color1u32(col[0]);
+
+	    GX_Position3f32(-1.0f,1.0f,1.0f);
+	    GX_Color1u32(col[1]);
+	    GX_Position3f32(1.0f,1.0f,1.0f);
+	    GX_Color1u32(col[1]);
+	    GX_Position3f32(1.0f,1.0f,-1.0f);
+	    GX_Color1u32(col[1]);
+	    GX_Position3f32(-1.0f,1.0f,-1.0f);
+	    GX_Color1u32(col[1]);
+
+	    GX_Position3f32(-1.0f,-1.0f,1.0f);
+	    GX_Color1u32(col[1]);
+	    GX_Position3f32(1.0f,-1.0f,1.0f);
+	    GX_Color1u32(col[1]);
+	    GX_Position3f32(1.0f,-1.0f,-1.0f);
+	    GX_Color1u32(col[1]);
+	    GX_Position3f32(-1.0f,-1.0f,-1.0f);
+	    GX_Color1u32(col[1]);
+
+	    GX_Position3f32(-1.0f,1.0f,1.0f);
+	    GX_Color1u32(col[2]);
+	    GX_Position3f32(-1.0f,1.0f,-1.0f);
+	    GX_Color1u32(col[2]);
+	    GX_Position3f32(-1.0f,-1.0f,-1.0f);
+	    GX_Color1u32(col[2]);
+	    GX_Position3f32(-1.0f,-1.0f,1.0f);
+	    GX_Color1u32(col[2]);
+
+	    GX_Position3f32(1.0f,1.0f,1.0f);
+	    GX_Color1u32(col[2]);
+	    GX_Position3f32(1.0f,1.0f,-1.0f);
+	    GX_Color1u32(col[2]);
+	    GX_Position3f32(1.0f,-1.0f,-1.0f);
+	    GX_Color1u32(col[2]);
+	    GX_Position3f32(1.0f,-1.0f,1.0f);
+	    GX_Color1u32(col[2]);
+        GX_End();
+
+	a+=0.5f;
+        GRRLIB_Render();
+    }
+    GRRLIB_Exit(); // Be a good boy, clear the memory allocated by GRRLIB
+
+    exit(0);
+}
+
--- examples/compositing/source/main.c
+++ examples/compositing/source/main.c
@@ -1,7 +1,5 @@
 /*===========================================
     NoNameNo Compositing Sample Code
-    Bugged since Screen2texture conversion
-    loose color precision.
 ============================================*/
 #include <grrlib.h>
 
@@ -37,14 +35,20 @@ int main() {
         WPAD_ScanPads();
         if(WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) exit(0);
 
-        GRRLIB_CompoStart();
+// we say thant we will want to capture to a texture all the following
+        GRRLIB_CompoStart(); 
+
             for(i=0;i<360;i+=30) {
+// We draw some letters
                 GRRLIB_DrawTile((rmode->fbWidth/2)-(tex_font->tilew/2), (rmode->efbHeight/2)-(tex_font->tileh+circsize), tex_font, rot+i, 1, 1, 0xFFFFFFFF, 65-32+((int)i/45));
             }
+
+// we say we want to capture now, (the buffer will be cleared after the capture)
         GRRLIB_CompoEnd(0, 0, tex_screen);
 
         rot-=0.6;
 
+// we now draw twice the captured buffer
         GRRLIB_DrawImg(50, 50, tex_screen, 0, 1, 1, 0xFFFFFFFF);
         GRRLIB_DrawImg(100, 100, tex_screen, 0, 1, 1, 0xFFFFFFFF);
 
