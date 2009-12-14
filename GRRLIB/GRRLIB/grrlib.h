7c0e6017b699fb2b18c8ca3e202671a60e0e8991
Crayon2000 <crayon1@rocketmail.com>
2009-12-14 04:30:05 +0000

    [CHG] Changed path to template code

--- GRRLIB/GRRLIB/grrlib.h
+++ GRRLIB/GRRLIB/grrlib.h
@@ -270,7 +270,7 @@ GRR_EXTERN  u32         fb       GRR_INIT(0);
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- * @example template/main.c
+ * @example template/source/main.c
  * This example shows the minimum code required to use GRRLIB.
  * It could be used as a template to start a new project.
  * More elaborate examples can be found inside the \e examples folder.
