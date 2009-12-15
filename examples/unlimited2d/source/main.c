37077c8da9103c218af1228229d5657de9a3a283
Crayon2000 <crayon1@rocketmail.com>
2009-12-15 15:53:58 +0000

    [CHG] Nothing important ;)

--- examples/unlimited2d/source/main.c
+++ examples/unlimited2d/source/main.c
@@ -21,14 +21,14 @@ extern GXRModeObj *rmode;
 int main() {
     int i;
     int screen_index = 0;
-    float t=0;
-    int R=81;
-    int r=71;
-    int d=120;
-    float f=0;
-    float ff=0;
-    float spr=0;
-    int n=1;
+    float t = 0;
+    const int R = 81;
+    const int r = 71;
+    const int d = 120;
+    float f = 0;
+    float ff = 0;
+    float spr = 0;
+    int n = 1;
 
 
     GRRLIB_Init();
@@ -47,14 +47,14 @@ int main() {
     GRRLIB_InitTileSet(tex_font, 16, 16, 32);
 
 
-    for(i=0;i<=255;i+=1){
+    for(i=0;i<=255;i+=1) {
         GRRLIB_Printf((640-(16*16))/2, 200, tex_font, 0xFFFFFF00|i, 1, "HOW MANY SPRITES");
         GRRLIB_Printf((640-(16*20))/2, 216, tex_font, 0xFFFFFF00|i, 1, "CAN YOU DISPLAY WITH");
         GRRLIB_DrawImg((640-352)/2, 248, tex_logo, 0, 1, 1, 0xFFFFFF00|i);
         GRRLIB_Printf((640-(16*28))/2, 480-16, tex_font, 0xFFFFFF00|i, 1, "BY NONAMENO FROM GRRLIB TEAM");
         GRRLIB_Render();
     }
-    for(i=255;i>=0;i-=2){
+    for(i=255;i>=0;i-=2) {
         GRRLIB_Printf((640-(16*16))/2, 200, tex_font, 0xFFFFFF00|i, 1, "HOW MANY SPRITES");
         GRRLIB_Printf((640-(16*20))/2, 216, tex_font, 0xFFFFFF00|i, 1, "CAN YOU DISPLAY WITH");
         GRRLIB_DrawImg((640-352)/2, 248, tex_logo, 0, 1, 1, 0xFFFFFF00|i);
@@ -73,27 +73,27 @@ int main() {
 
         GRRLIB_Render();
         screen_index++;
-        screen_index %= 10;		
-        spr+=0.1f;	
+        screen_index %= 10;
+        spr+=0.1f;
         t+=0.01f;
 
-        if(t>n*2*PI){
+        if(t>n*2*PI) {
             n++;
             f+=0.01f;
         }
 
-        if(f>2*PI){
+        if(f>2*PI) {
             f=0;
             ff+=0.02f;
         }
 
 
         if(WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) exit(0);
-        
+
     }
     GRRLIB_Exit(); // Be a good boy, clear the memory allocated by GRRLIB
     GRRLIB_FreeTexture(tex_logo);
-    GRRLIB_FreeTexture(tex_ball);   
+    GRRLIB_FreeTexture(tex_ball);
     GRRLIB_FreeTexture(tex_font);
     for(i=0; i<10; i++) {
         GRRLIB_FreeTexture(tex_screen[i]);
