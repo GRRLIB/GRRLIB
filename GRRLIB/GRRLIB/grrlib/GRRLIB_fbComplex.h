5deb38d50276386a03a5281420dfd3c82b352e0c
Crayon2000 <crayon1@rocketmail.com>
2010-01-04 03:48:33 +0000

    [CHG] Copyright date changed to 2010

--- GRRLIB/GRRLIB/grrlib/GRRLIB_fbComplex.h
+++ GRRLIB/GRRLIB/grrlib/GRRLIB_fbComplex.h
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------------------
-Copyright (c) 2009 The GRRLIB Team
+Copyright (c) 2010 The GRRLIB Team
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
