1012d5a10186089c463f268ab6f55019fb836426
Crayon2000 <crayon1@rocketmail.com>
2010-01-18 05:02:30 +0000

    [CHG] pngtest.c was deleted (not needed to build libpng)

--- GRRLIB/GRRLIB/GRRLIB_3D.c
+++ GRRLIB/GRRLIB/GRRLIB_3D.c
@@ -294,8 +294,7 @@ void GRRLIB_DrawSphere(f32 r, int lats, int longs, bool filled) {
  * @param size Size of the cube edge.
  * @param filled Wired or not.
 */
-void GRRLIB_DrawCube(f32 size, bool filled)
-{
+void GRRLIB_DrawCube(f32 size, bool filled) {
     static f32 n[6][3] =
     {
         {-1.0, 0.0, 0.0},
