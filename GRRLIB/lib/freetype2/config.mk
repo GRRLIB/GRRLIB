07a6685a0e4601168ff3fe1bb809ca3df6dc9fa5
Crayon2000 <crayon1@rocketmail.com>
2010-02-15 23:53:41 +0000

    [NEW] Including FreeType 2.3.12 source code (not fully tested and not incorporated into main Makefile)

---
+++ GRRLIB/lib/freetype2/config.mk
@@ -0,0 +1,30 @@
+#
+# FreeType 2 configuration rules
+#
+
+
+# Copyright 1996-2000 by
+# David Turner, Robert Wilhelm, and Werner Lemberg.
+#
+# This file is part of the FreeType project, and may only be used, modified,
+# and distributed under the terms of the FreeType project license,
+# LICENSE.TXT.  By continuing to use, modify, or distribute this file you
+# indicate that you have read the license and understand and accept it
+# fully.
+
+DELETE    := rm -f
+SEP       := /
+
+COMPILER_SEP := $(SEP)
+
+# Library linking
+#
+CLEAN_LIBRARY ?= $(DELETE) $(subst /,$(SEP),$(PROJECT_LIBRARY))
+LINK_LIBRARY   = $(AR) -r $@ $(subst /,$(COMPILER_SEP),$(OBJECTS_LIST))
+
+include $(TOP_DIR)/builds/wii/wii-def.mk
+include $(TOP_DIR)/builds/compiler/wii-cc.mk
+include $(TOP_DIR)/builds/link_std.mk
+
+
+# EOF
