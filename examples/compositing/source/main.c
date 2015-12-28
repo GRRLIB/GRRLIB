ec8af365c566eb0253e5d0d3a4426334a3f60cd5
Crayon2000 <crayon1@rocketmail.com>
2015-12-28 17:00:17 -0500

    Updated Compositing sample code.
    Removed extra header, corrected typo and coding standard.

--- examples/compositing/source/main.c
+++ examples/compositing/source/main.c
@@ -4,13 +4,10 @@
 #include <grrlib.h>
 
 #include <stdlib.h>
-#include <math.h>
-#include <malloc.h>
 #include <wiiuse/wpad.h>
 
 #include "gfx/font3d.h"
 extern GXRModeObj *rmode;
-
 
 
 int main() {
@@ -25,10 +22,9 @@ int main() {
 
     GRRLIB_texImg *tex_font = GRRLIB_LoadTexture(font3d);
     GRRLIB_InitTileSet(tex_font, 64, 64, 32);
-    GRRLIB_SetHandle (tex_font, tex_font->tilew/2, tex_font->tileh+circsize);
+    GRRLIB_SetHandle(tex_font, tex_font->tilew/2, tex_font->tileh+circsize);
 
-    GRRLIB_texImg *tex_screen;
-    tex_screen = GRRLIB_CreateEmptyTexture(rmode->fbWidth,rmode->efbHeight);
+    GRRLIB_texImg *tex_screen = GRRLIB_CreateEmptyTexture(rmode->fbWidth,rmode->efbHeight);
 
     GRRLIB_Settings.antialias = true;
 
@@ -36,10 +32,10 @@ int main() {
         WPAD_ScanPads();
         if(WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) exit(0);
 
-// we say thant we will want to capture to a texture all the following
+// we say that we will want to capture to a texture all the following
         GRRLIB_CompoStart();
 
-            for(i=0;i<360;i+=30) {
+            for(i=0; i<360; i+=30) {
 // We draw some letters
                 GRRLIB_DrawTile((rmode->fbWidth/2)-(tex_font->tilew/2), (rmode->efbHeight/2)-(tex_font->tileh+circsize), tex_font, rot+i, 1, 1, 0xFFFFFFFF, text[(int)(i/30)]-32);
             }
@@ -49,7 +45,7 @@ int main() {
 
         rot-=0.6;
 
-// we now draw 3 times  the captured buffer playing with color
+// we now draw 3 times the captured buffer playing with color
         GRRLIB_DrawImg(0, 0, tex_screen, 0, 1, 1, 0xFF00FFFF);
         GRRLIB_DrawImg(50, 50, tex_screen, 0, 1, 1, 0xFFFF00FF);
         GRRLIB_DrawImg(100, 100, tex_screen, 0, 1, 1, 0xFFFFFFFF);
