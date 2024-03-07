fd14bb852636c121b448322f77454733e3409867
Crayon2000 <crayon1@rocketmail.com>
2024-03-07 18:56:06 +0000

    Deploying to gh-pages from @ GRRLIB/GRRLIB@159ba73f5c82076e883c8b09172290e2fcb16218 🚀

--- search/all_5.js
+++ search/all_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['face_0',['face',['../struct_g_r_r_l_i_b___font.html#a22e9f2a8a4fc5c0f350faffdfdc1ef54',1,'GRRLIB_Font']]]
+  ['face_0',['face',['../struct_g_r_r_l_i_b___font.html#a22e9f2a8a4fc5c0f350faffdfdc1ef54',1,'GRRLIB_Font']]],
+  ['format_1',['format',['../struct_g_r_r_l_i_b__tex_img.html#a8fb1c5212011391e7e4e75f750ed3a36',1,'GRRLIB_texImg']]]
 ];
